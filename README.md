dbaea099ed2807330ced27e7070ec28055c6c579
0xPr0f <94965754+0xPr0f@users.noreply.github.com>
2022-01-20 11:00:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Unstoppable NFT
+
+## This is a project by 0xProf for the Unstoppable domain hackathon
+
+Unstoppable NFT is a domain gated application
+i.e you must own a unstoppable domain to access it
+this idea was inspired by token gating but then to encourage users to get a UD domain
+
+## How is works
+
+You will log into a page where you will confirm your ownership of a UD domain
+then after confirmation you will be automatically be redirected to the DApp
+
+## Submission details
+
+_Project Name: Unstoppable NFT_  
+_Code Repo: https://github.com/0xPr0f/Unstoppable-NFT_  
+_< 3mins Video: https://youtu.be/OBL5X-OXwrY_  
+_Discord ID: 0xProf#7686_  
+_UD email address: 0xprofoz@gmail.com_
+
+## Extras  
+Longer video/Detailed explanation: https://youtu.be/SUidrXPVeeY
---
+++ src/Nav.js
@@ -0,0 +1,42 @@
+//import './styles/global.css'
+import { Link, BrowserRouter, Route, Switch } from "react-router-dom";
+
+import XplayVids from "./XplayVid";
+
+function Navhan() {
+  return (
+    <>
+      <BrowserRouter>
+        <div
+          className="App overflow-visible"
+          style={{
+            height: "65px",
+            display: "flex",
+            background: "#000",
+            color: "#fff",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>{/*  <img width={200} height={200} src="/Meta.png" /> */}</div>
+
+          <div>
+            <Link
+              style={{ padding: "0rem 3rem" }}
+              className="text-0.05px text-blue-500 md:text-0.01px"
+              to="./XplayVid"
+            >
+              Play Vids
+            </Link>
+          </div>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/XplayVid" component={XplayVids} exact></Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+export default Navhan;
